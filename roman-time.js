8358d3f077539a0cb5112cdb504b0f3234d1fc9c
mangina <mangina.yulya@gmail.com>
2015-10-07 20:56:08 +0500

    исправила все ошибки

--- roman-time.js
+++ roman-time.js
@@ -4,7 +4,7 @@ var minutes = Number(process.argv[3]);
 var arab = [1, 4, 5, 9, 10, 40, 50];
 var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
 
-function arabInRoman(number) {
+function arabToRoman(number) {
     var result = '';
     var i = arab.length - 1;
 
@@ -26,32 +26,32 @@ function arabInRoman(number) {
 
 var checkRange = function (number, min, max) {
     if (number >= min && number <= max) {
-        return true;
+        return number >= min && number <= max;
     }
-    return false;
+
 };
 
 function getRomanTime(hours, minutes) {
-    if (!checkRange(hours, 1, 24) || !checkRange(minutes, 0, 59)) {
-        console.log('Вы ошиблись.');
-        console.log('node less1 1<hours<24 0<minutes<59');
+    if (!checkRange(hours, 0, 23) || !checkRange(minutes, 0, 59)) {
+        console.log('Время указано не верно.');
+        console.log('node less1 0<hours<23 0<minutes<59');
         return;
     }
 
-    return arabInRoman(hours) + ':' + arabInRoman(minutes);
+    return arabToRoman(hours) + ':' + arabToRoman(minutes);
 }
 
 console.log(getRomanTime(hours, minutes));
 /*
-console.log("time ", getRomanTime(0, 0));
-console.log("time ", getRomanTime(24, 0));
-console.log("time ", getRomanTime(0, 24));
-console.log("time ", getRomanTime(0, 60));
-console.log("time ", getRomanTime(24, 24));
-console.log("time ", getRomanTime(10, 10));
-console.log("time ", getRomanTime(0, 59));
-console.log("time ", getRomanTime(59, 0));
-console.log("time ", getRomanTime(60, 0));
-console.log("time ", getRomanTime(24, 60));
-console.log("time ", getRomanTime(24, 59));
+console.log("time1 ", getRomanTime(0, 0));
+console.log("time2 ", getRomanTime(24, 0));
+console.log("time3 ", getRomanTime(0, 24));
+console.log("time4 ", getRomanTime(0, 60));
+console.log("time5 ", getRomanTime(24, 24));
+console.log("time6 ", getRomanTime(10, 10));
+console.log("time7 ", getRomanTime(0, 59));
+console.log("time8 ", getRomanTime(59, 0));
+console.log("time9 ", getRomanTime(60, 0));
+console.log("time10 ", getRomanTime(24, 60));
+console.log("time11 ", getRomanTime(23, 59));
 */
